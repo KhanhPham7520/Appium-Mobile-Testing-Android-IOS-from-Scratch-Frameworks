abd471e765590b78a20edafc2ef64b0134c5acdd
Pham Phan Nhat Khanh <phamphannhatkhanh7520@gmail.com>
2022-12-10 15:22:53 +0700

    Refactor code

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -3,7 +3,10 @@ package com.qa.tests;
 import com.qa.BaseTest;
 import com.qa.pages.LoginPage;
 import com.qa.pages.ProductPage;
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
@@ -22,8 +25,10 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void invalidUserName(){
-        loginPage.enterPassword("invalidusername");
+        loginPage.enterUsername("invalidusername");
         loginPage.enterPassword("secret_sauce");
         productPage = loginPage.pressLoginBtn();
+        Assert.assertEquals(loginPage.getErrTxt(),
+                "Username and password do not match any user in this service.");
     }
 }
